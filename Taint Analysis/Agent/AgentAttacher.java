fc9849743b1ed07368c20a06d0b7312a9bbf0687
Melicious <126588135+hviet03@users.noreply.github.com>
2024-10-16 17:04:37 +0700

    Update AgentAttacher.java

--- Taint Analysis/Agent/AgentAttacher.java
+++ Taint Analysis/Agent/AgentAttacher.java
@@ -19,21 +19,21 @@ public class AgentAttacher {
             e.printStackTrace();
         }
 
-//       Search PID cua JVM (chua xong, thuc hien loi)
-//        for (VirtualMachineDescriptor vmd : VirtualMachine.list()) {
-//            System.out.println("PID: " + vmd.id() + ", Main class: " + vmd.displayName());
-//            // Target vào PID nhất định: String targetPid = "";
-//            try {
-//                // Attach vào tất cả JVM
-//                VirtualMachine vm = VirtualMachine.attach(vmd.id());  // VirtualMachine vm = VirtualMachine.attach(targetPid);
-//                // Load agent
-//                vm.loadAgent(agentPath);
-//                vm.detach();
-//                System.out.println("Agent attached successfully to JVM with PID: " + vmd.id());
-//            } catch (Exception e) {
-//                System.err.println("Failed to attach to JVM with PID: " + vmd.id());
-//                e.printStackTrace();
-//            }
-//        }
+
+       for (VirtualMachineDescriptor vmd : VirtualMachine.list()) {
+            System.out.println("PID: " + vmd.id() + ", Main class: " + vmd.displayName());
+            // Target vào PID nhất định: String targetPid = "";
+            try {
+                // Attach vào tất cả JVM
+                VirtualMachine vm = VirtualMachine.attach(vmd.id());  // VirtualMachine vm = VirtualMachine.attach(targetPid);
+                // Load agent
+                vm.loadAgent(agentPath);
+                vm.detach();
+                System.out.println("Agent attached successfully to JVM with PID: " + vmd.id());
+            } catch (Exception e) {
+                System.err.println("Failed to attach to JVM with PID: " + vmd.id());
+                e.printStackTrace();
+            }
+        }
     }
 }
